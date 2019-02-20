e70e9734bdb90eb9f27db01530485f4a4f7bf022
Zach Ohara <zjohara@gmail.com>
2019-02-19 21:12:57 -0800

    Small fixes to ParamValueLabel

--- ParamValueLabel.cpp
+++ ParamValueLabel.cpp
@@ -1,9 +1,10 @@
 #include "ParamValueLabel.h"
 
 ParamValueLabel::ParamValueLabel(IPlugBase* pPlug, int paramIdx, IRECT pR) :
-	ITextControl(pPlug, pR, &defaultParamText, "Hello PVL!")
+	ITextControl(pPlug, pR, &defaultParamText, "")
 {
 	mpParam = pPlug->GetParam(paramIdx);
+	UpdateDisplay();
 }
 
 ParamValueLabel::~ParamValueLabel()
@@ -12,11 +13,11 @@ ParamValueLabel::~ParamValueLabel()
 
 void ParamValueLabel::UpdateDisplay()
 {
-	char* display = (char*)malloc(MAX_PARAM_DISPLAY_LEN);
-	mpParam->GetDisplayForHost(display);
+	char* display[MAX_PARAM_DISPLAY_LEN];
+	mpParam->GetDisplayForHost((char*) display);
 	const char* label = mpParam->GetLabelForHost();
-	strcat(display, label);
-	SetText(display);
+	strcat((char*) display, label);
+	SetText((char*) display);
 }
 
 void ParamValueLabel::SetText(char* text)
